5aa962395bcb398e534c5155e291d96d2cb51dcb
NicKarlen <nicolas.k@gmx.net>
2022-06-21 21:22:54 +0200

    add: BOJ
    refactor needed!

--- main.py
+++ main.py
@@ -111,3 +111,33 @@ def get_data_ECB():
     df.columns=['Date', 'Total Assets']
 
     return format_df(df)
+
+def get_data_BOJ():
+    # get timestamp
+    time = datetime.now().strftime("%Y-%m-%d")
+
+    # URL direction to the Data for the ECB BalanceSheet. 
+    url = f"""https://fred.stlouisfed.org/graph/fredgraph.csv?bgcolor=%23e1e9f0&chart_type=line&drp=0&fo
+            =open%20sans&graph_bgcolor=%23ffffff&height=450&mode=fred&recession_bars=off&txtcolor=%23444
+            444&ts=12&tts=12&width=1168&nt=0&thu=0&trc=0&show_legend=yes&show_axis_titles=yes&show_toolt
+            ip=yes&id=JPNASSETS&scale=left&cosd=1998-04-01&coed=2022-05-01&line_color=%234572a7&link_val
+            ues=false&line_style=solid&mark_type=none&mw=3&lw=2&ost=-99999&oet=99999&mma=0&fml=a&fq=Mont
+            hly%2C%20End%20of%20Period&fam=avg&fgst=lin&fgsnd=2020-02-01&line_index=1&transformation=lin
+            &vintage_date={time}&revision_date={time}&nd=1998-04-01"""
+
+    # Request the data
+    req = requests.get(url)
+
+    # Write the requested data to a .xml file
+    with open("Data/data_boj.csv", "w", encoding="utf-8") as f:
+        f.write(req.text)
+
+    # read the just downloaded csv in to a dataframe
+    df = pd.read_csv("Data/data_boj.csv",sep="," , header=0)
+
+    # Rename columns
+    df.columns=['Date', 'Total Assets']
+
+    return format_df(df)
+
+
--- plot.py
+++ plot.py
@@ -5,14 +5,16 @@ import matplotlib.pyplot as plt
 df_ecb = main.get_data_ECB()
 df_snb = main.get_data_SNB()
 df_fed = main.get_data_FED()
+df_boj = main.get_data_BOJ()
 
 fig, ax = plt.subplots()
 
 df_ecb.plot(kind='line', x='Date', y='Total Assets', xlabel='Date', ylabel='Millions', legend='True', ax=ax)
 df_snb.plot(kind='line', x='Date', y='Total Assets', xlabel='Date', ylabel='Millions', legend='True', ax=ax)
 df_fed.plot(kind='line', x='Date', y='Total Assets', xlabel='Date', ylabel='Millions', legend='True', ax=ax)
+df_boj.plot(kind='line', x='Date', y='Total Assets', xlabel='Date', ylabel='Millions', legend='True', ax=ax)
 
-ax.legend(['ECB', 'SNB', 'FED'])
+ax.legend(['ECB', 'SNB', 'FED', 'BOJ'])
 
 fig.suptitle('Central Bank Assets')
 
